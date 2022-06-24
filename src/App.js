f7a5d03f9933b76e4e1f0d97ebc76b617a656cc7
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-06-24 14:01:56 +0600

    motion

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import Portfolio from "./Component/Portfolio/Portfolio";
 import Testimonial from "./Component/Testimonial/Testimonial";
 import Contact from "./Component/Contact/Contact";
 import Footer from "./Component/Footer/Footer";
+import { themeContext } from "./Context";
+import { useContext } from "react";
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
       <Navbar />
       <Intro />
       <Service />
--- src/Component/Toogle/Toogle.jsx
+++ src/Component/Toogle/Toogle.jsx
@@ -3,12 +3,23 @@ import React from "react";
 import "./Toogle.css";
 import UilSun from "@iconscout/react-unicons/icons/uil-sun";
 import UilMoon from "@iconscout/react-unicons/icons/uil-moon";
+import { themeContext } from "../../Context";
+import { useContext } from "react";
 const Toogle = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  const handelClick = () => {
+    theme.dispatch({ type: "toggle" });
+  };
   return (
     <div className="toogle">
       <UilSun />
       <UilMoon />
-      <div className="t-button"></div>
+      <div
+        className="t-button"
+        onClick={handelClick}
+        style={darkMode ? { left: "2px" } : { right: "2px" }}
+      ></div>
     </div>
   );
 };
